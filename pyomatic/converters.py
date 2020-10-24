9c6e681684eb1b0f54c7d9e3cc55a7af17628d16
Tim Kopp <tim.kopp@cyber-balance.com>
2020-10-24 14:30:31 -0400

    added unit tests

--- pyomatic/converters.py
+++ pyomatic/converters.py
@@ -4,8 +4,10 @@ class Convert:
         self.name=''
     def to_percent(self, num1, num2):  
         return round((num1/num2)*100,2)
+    def to_percentf(self, num1, num2):  
+        return '{0:.0f}%'.format(self.to_percent( num1, num2))
     def to_des(self):  
-        return HTML('<img src= ttps://ca.slack-edge.com/T01ADCEKEDR-U01BFB7LFC0-8334c324cf2c-512" />' )   
+        return HTML('<img src="https://ca.slack-edge.com/T01ADCEKEDR-U01BFB7LFC0-8334c324cf2c-512" />' )   
        
 
 
--- unittests/compare_tests.py
+++ unittests/compare_tests.py
@@ -2,12 +2,13 @@ import unittest
 from pyomatic.converters import Convert  
 
 class MyTest(unittest.TestCase):
-    def test(self):
-        _convert = Convert()
-        per = Convert().to_percent(2,4)
-        print(per)
+    def test_to_percent(self): 
+        per = Convert().to_percent(2,4) 
         self.assertEqual(per, 50.0)
-        
+
+    def test_to_percentf(self): 
+        per = Convert().to_percentf(2,4)  
+        self.assertEqual(per, '50%')        
         
 if __name__ == '__main__':
     unittest.main()        
